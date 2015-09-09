291c4cc82e2b98681a92442f1c3eb1a0a052fff1
RaceProUK <raceprouk@hotmail.co.uk>
2015-09-09 20:23:38 +0000

    Functionally complete (in theory)

--- Emoji.js
+++ Emoji.js
@@ -6,15 +6,32 @@
  * @license MIT
  */
 
+const mapping = require('./Emoji.json');
+
+const internals = {
+    browser: null
+};
+
+const privateFns = {
+    handler: handler,
+    replaceEmoji: replaceEmoji
+};
+
 /**
  * Prepare Plugin prior to login
  *
- * @param {*} plugConfig Plugin specific configuration
- * @param {Config} config Overall bot configuration
- * @param {externals.events.SockEvents} events EventEmitter used for the bot
- * @param {Browser} browser Web browser for communicating with discourse
+ * @param {*} pluginConfig Plugin specific configuration
+ * @param {SockBot.Config} botConfig Overall bot configuration
+ * (see [SockBot docs](https://sockbot.rtfd.org/en/latest/api/lib/config/) for more details)
+ * @param {SockBot.SockEvents} events EventEmitter used for the bot
+ * (see [SockBot docs](https://sockbot.rtfd.org/en/latest/api/external/events/#module_SockEvents) for more details)
+ * @param {SockBot.Browser} browser Web browser for communicating with discourse
+ * (see [SockBot docs](https://sockbot.rtfd.org/en/latest/api/lib/browser/) for more details)
  */
-exports.prepare = function (plugConfig, config, events, browser) {};
+exports.prepare = function (pluginConfig, botConfig, events, browser) {
+    internals.browser = browser;
+    events.onChannel('/latest', handler);
+};
 
 /**
  * Start the plugin after login
@@ -27,10 +44,43 @@ exports.start = function () {};
 exports.stop = function () {};
 
 /**
- * Handle notifications
+ * Replace emoji in the user's own posts
  *
- * @param {external.notifications.Notification} notification Notification recieved
- * @param {external.topics.Topic} topic Topic trigger post belongs to
- * @param {external.posts.CleanedPost} post Post that triggered notification
+ * @param {SockBot.Message} message Notification recieved
+ * (see [SockBot docs](https://sockbot.rtfd.org/en/latest/api/external/messageBus/#external.module_messageBus..message)
+ * for more details)
  */
-exports.handler = function handler(notification, topic, post) {};
+function handler(message) {
+    internals.browser.getLastPosts(message.topic_id, replaceEmoji, () => 0);
+};
+
+/**
+ * Replace emoji in the user's own post
+ *
+ * @param {SockBot.CleanedPost} post The post to process
+ * (see [SockBot docs](https://sockbot.rtfd.org/en/latest/api/external/messageBus/#external.module_messageBus..message)
+ * for more details)
+ * @param {SockBot.completedCallback} callback Completion callback
+ */
+function replaceEmoji(post, callback) {
+    if (!post.yours) {
+        callback();
+    } else {
+        let edited = post.raw;
+        for (let emoji in mapping) { //eslint-disable-line prefer-const
+            edited = edited.replace(emoji, mapping[emoji]);
+        }
+        if (edited === post.raw)  {
+            callback();
+        } else {
+            internals.browser.editPost(post.id, edited, null, callback);
+        }
+    }
+};
+
+/* istanbul ignore else */
+if (typeof GLOBAL.describe === 'function') {
+    //test is running
+    exports.internals = internals;
+    exports.privateFns = privateFns;
+}
--- test/EmojiTest.js
+++ test/EmojiTest.js
@@ -1,12 +1,14 @@
 'use strict';
-/*globals describe, it*/
+/*globals describe, it, before, afterEach*/
 
 const chai = require('chai'),
     sinon = require('sinon');
 chai.should();
 const expect = chai.expect;
 
-const emoji = require('../Emoji');
+const emoji = require('../Emoji'),
+    mapping = require('../Emoji.json');
+
 describe('Emoji', () => {
     it('should export prepare()', () => {
         expect(emoji.prepare).to.be.a('function');
@@ -17,13 +19,61 @@ describe('Emoji', () => {
     it('should export stop()', () => {
         expect(emoji.stop).to.be.a('function');
     });
-    it('should export handler()', () => {
-        expect(emoji.handler).to.be.a('function');
-    });
     it('should have start() as a stub function', () => {
         expect(emoji.start).to.not.throw();
     });
     it('should have stop() as a stub function', () => {
         expect(emoji.stop).to.not.throw();
+    });
+    it('should export handler()', () => {
+        expect(emoji.privateFns.handler).to.be.a('function');
+    });
+    it('should export replaceEmoji()', () => {
+        expect(emoji.privateFns.replaceEmoji).to.be.a('function');
+    });
+    describe('prepare()', () => {
+        it('should register listener for `/latest`', () => {
+            const spy =  sinon.spy();
+            emoji.prepare(null, null, {onChannel: spy}, null);
+            spy.calledWith('/latest', emoji.privateFns.handler).should.be.true;
+        });
+    });
+    describe('handler()', () => {
+        it('should call browser.getLastPosts()', () => {
+            const spy = sinon.spy((_, __, callback) => callback());
+            emoji.internals.browser = {getLastPosts: spy};
+            emoji.privateFns.handler({'topic_id': 1});
+            spy.calledOnce.should.be.true;
+            expect(spy.args[0][0]).to.equal(1);
+            expect(spy.args[0][1]).to.equal(emoji.privateFns.replaceEmoji);
+            expect(spy.args[0][2]).to.be.a('function');
+        });
+    });
+    describe('replaceEmoji()', () => {
+        const spy = sinon.spy((_, __, ___, callback) => callback());
+        const dummy = () => 0;
+        before(() => {
+            emoji.internals.browser = {editPost: spy};
+        });
+        afterEach(() =>{
+            spy.reset();
+        });
+        Object.keys(mapping).forEach(raw => {
+            it('should replace ' + raw + ' with ' + mapping[raw], () => {
+                emoji.privateFns.replaceEmoji({id: 1, raw: raw, yours: true}, dummy);
+                spy.calledOnce.should.be.true;
+                expect(spy.args[0][0]).to.equal(1);
+                expect(spy.args[0][1]).to.equal(mapping[raw]);
+                expect(spy.args[0][3]).to.be.a('function');
+            });
+        });
+        it('should not call browser.editPost() when nothing changed', () => {
+            emoji.privateFns.replaceEmoji({id: 1, raw: 'text', yours: true}, dummy);
+            spy.called.should.be.false;
+        });
+        it('should not call browser.editPost() when not your post', () => {
+            emoji.privateFns.replaceEmoji({id: 1, raw: 'text', yours: false}, dummy);
+            spy.called.should.be.false;
+        });
     });
 });
